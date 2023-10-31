601334df8a5ddc8a16fb094725f449e211c47d15
Ronnie Ayesigwa <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-10-31 10:45:40 +0300

    Update page.tsx

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -2,8 +2,16 @@ import { EmployeesClient } from "./components/client";
 import { connectToDB } from "@/lib/mongoose";
 import Employee from "@/models/employee.model";
 import { EmployeeColumn } from "./components/columns";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 const Employees = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/server");
+  }
   await connectToDB();
   const employees = await Employee.find();
   // console.log(employees);
