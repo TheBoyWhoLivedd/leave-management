2d2a8167e676305a28f580a3f9202eb78273f281
Ronnie Ayesigwa <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-11-03 10:32:20 +0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,40 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/options";
+import { CustomSession, authOptions } from "../api/auth/[...nextauth]/options";
+import { connectToDB } from "@/lib/mongoose";
+import LeaveBalance from "@/models/leaveBalance.model";
+
 const page = async () => {
-  const session = await getServerSession(authOptions);
+  const session: CustomSession | null = await getServerSession(authOptions);
   console.log(session);
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/server");
   }
+  await connectToDB();
+  const { Balance } = await LeaveBalance.findOne({
+    Employee: session?.user?.id,
+  });
 
-  return <div>{session?.user?.name}</div>;
+  const date = new Date();
+  const today = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "long",
+  }).format(date);
+
+  return (
+    <div className="shadow hover:shadow-lg dark:shadow-lg dark:bg-slate-900 rounded-lg p-6 mx-auto max-w-2xl mt-10">
+      <h6 className="text-gray-500 text-xs font-medium uppercase tracking-wider">
+        {today}
+      </h6>
+      <h1 className="text-2xl font-semibold mt-2">
+        Welcome {session?.user?.name}
+      </h1>
+      <p className=" mt-4">
+        You have <span className="font-semibold">{Balance}</span> days of Leave
+        left
+      </p>
+    </div>
+  );
 };
 
 export default page;
