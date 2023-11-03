e30738c499358b278126f68521560045912b7f70
Ronnie Ayesigwa <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-11-03 16:30:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Login from "@/components/login";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
+    <main className="flex min-h-screen items-center justify-center p-4 md:p-24">
       <Login />
     </main>
   );
